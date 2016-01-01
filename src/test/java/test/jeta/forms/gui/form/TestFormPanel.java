542bdb242d949837508656486cbea7e58d420b2a
Jeff Tassin <jtassin@corp.acesse.com>
2015-12-31 19:10:35 -0600

    Added test.

---
+++ src/test/java/test/jeta/forms/gui/form/TestFormPanel.java
@@ -0,0 +1,30 @@
+package test.jeta.forms.gui.form;
+
+import java.awt.event.WindowAdapter;
+
+import javax.swing.JFrame;
+
+import com.jeta.forms.components.panel.FormPanel;
+import com.sun.glass.events.WindowEvent;
+
+
+public class TestFormPanel {
+
+	public static void main(String[] args ) {
+		System.out.println( "starting test...");
+		JFrame frame = new JFrame();
+		FormPanel panel = new FormPanel("com/jeta/swingbuilder/gui/main/columnSpec.frm");
+		frame.getContentPane().add(panel);
+
+		// set the size and location of this frame
+		frame.setSize(600, 500);
+		frame.setLocation(200, 100);
+		frame.setVisible(true);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				System.exit(0);
+			}
+		});
+	}
+
+}
